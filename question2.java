2c76ade51ce51239576102ab76d62884618d7552
AlexFloresVeliz <floresv.alex@gmail.com>
2019-03-07 19:20:55 -0500

    initial commit

---
+++ question2.java
@@ -0,0 +1,13 @@
+package os_hw0;
+
+public class question2 {
+	
+//	Write a multithreaded program that outputs prime numbers. This
+//	program should work as follows: The user will run the program and
+//	will enter a number on the command line. The program will then create
+//	a separate thread that outputs all the prime numbers less than or equal
+//	to the number entered by the user. (You can use Java or C)
+	
+	//https://github.com/LC-CUNY/Programming_HW0/blob/master/Programming%20hw.pdf
+
+}
