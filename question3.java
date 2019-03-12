7b216742dfe84817202c4dc2cd8d54fd7fed6302
JordanFalcon <j.falcon122@gmail.com>
2019-03-12 19:38:55 -0400

    Begin class

--- question3.java
+++ question3.java
@@ -1,7 +1,6 @@
-package os_hw0;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class question3 {
-	
 //	Write a multithreaded program that calculates various statistical values
 //	for a list of numbers. This program will be passed a series of numbers on
 //	the command line and will then create three separate worker threads.
@@ -23,4 +22,58 @@ public class question3 {
 	
 	//https://github.com/LC-CUNY/Programming_HW0/blob/master/Programming%20hw.pdf
 
-}
+public class question3 {
+    public static void main(String[] args) 
+    { 
+        System.out.println("Enter numbers separated by a comma. Ex: 2, 433, 54, 665, 44, 2");   
+        Scanner scanner = new Scanner(System.in);
+        String numScanned = scanner.nextLine();
+        //System.out.println(numScanned);
+
+        parser(numScanned);
+    } 
+
+    public static void parser(String num){
+        String[] numStrArray = num.split(",");
+        int[] numArray = new int[numStrArray.length];
+
+        for (int i = 0; i < numStrArray.length; i++ ) {
+            numArray[i] = Integer.parseInt(numStrArray[i]);
+        }
+        
+
+        System.out.println("Maximum: "+numMax(numArray));
+        System.out.println("Minimum: "+numMin(numArray));
+        System.out.println("Average: "+numMean(numArray));
+    }
+
+    public static int numMax(int numArr[]) {
+        int max = numArr[0];
+        for (int i = 0; i < numArr.length; i++ ) {
+            if (numArr[i] > max) {
+                max = numArr[i];
+            }
+        }
+            return max;
+    }
+
+    public static int numMin(int numArr[]) {
+        int min = numArr[0];
+        for (int i = 0; i < numArr.length; i++ ) {
+            if (numArr[i] < min) {
+                min = numArr[i];
+            }
+        }
+            return min;
+    }
+
+    public static int numMean(int numArr[]) {
+        int sum = 0;
+        for (int i = 0; i < numArr.length; i++ ) {
+            sum += numArr[i];
+        }
+
+        return (sum/numArr.length);
+    }
+
+} 
